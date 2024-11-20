5fe961720b1a37f4bd8b0a6d0cd94a4f682b7ee7
Adam Ellis <adamellis@mac.lan>
2024-11-20 20:58:17 +0000

    Initial commit

---
+++ HomeSchool/HomeSchool/HomeSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeSchoolApp.swift
+//  HomeSchool
+//
+//  Created by Adam Ellis on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct HomeSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
